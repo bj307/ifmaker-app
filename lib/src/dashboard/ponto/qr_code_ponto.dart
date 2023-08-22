8a126969dd21f384faba0a9e8d37b5e9f401edc0
kaio <kaioresendeoficial@outlook.com>
2023-08-21 22:36:29 -0300

    :sparkles: tela ler qrcode :sparkles:

--- lib/src/dashboard/ponto/qr_code_ponto.dart
+++ lib/src/dashboard/ponto/qr_code_ponto.dart
@@ -1,7 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class QrCodePonto extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class QrCodePonto extends StatefulWidget {
   const QrCodePonto({super.key});
+
+  @override
+  State<QrCodePonto> createState() => _QrCodePontoState();
+}
+
+class _QrCodePontoState extends State<QrCodePonto> {
+  List<CameraDescription> cameras = [];
+  CameraController? controller;
+  XFile? imagem;
+  Size? size;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    try {
+      cameras = await availableCameras();
+      _startCamera();
+    } on CameraException catch (e) {
+      print(e.description);
+    }
+  }
+
+  _startCamera() {
+    if (cameras.isEmpty) {
+      print("Camera não encontrada");
+    } else {
+      _previewCamera(cameras.first);
+    }
+  }
+
+  _previewCamera(CameraDescription camera) async {
+    final CameraController cameraController = CameraController(
+        camera, ResolutionPreset.high,
+        enableAudio: false, imageFormatGroup: ImageFormatGroup.jpeg);
+    controller = cameraController;
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      print(e.description);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,13 +81,13 @@ class QrCodePonto extends StatelessWidget {
             child: Padding(
                 padding: const EdgeInsets.fromLTRB(16, 16, 16, 28),
                 child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white, // Cor de fundo
-                    borderRadius: BorderRadius.circular(5),
-                    border:
-                        Border.all(color: const Color(0xFF4CB050), width: 1.0),
-                  ),
-                )),
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Cor de fundo
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                          color: const Color(0xFF4CB050), width: 1.0),
+                    ),
+                    child: _arquivoWidget())),
           ),
           //botao registrar
           Padding(
@@ -91,6 +144,26 @@ class QrCodePonto extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  _cameraPreviewWidget() {
+    final CameraController? cameraController = controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return const Text('Erro ao abrir a câmera');
+    } else {
+      return AspectRatio(
+        aspectRatio: 1.0, // Proporção quadrada (1:1)
+        child: CameraPreview(controller!),
+      );
+    }
+  }
+
+  _arquivoWidget() {
+    return Container(
+      child: imagem == null
+          ? _cameraPreviewWidget()
+          : Image.file(File(imagem!.path), fit: BoxFit.contain),
     );
   }
 }
