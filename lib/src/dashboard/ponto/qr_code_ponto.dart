3172f653ee0fd612cc431fc442b2a72db65df3e0
kaio <kaioresendeoficial@outlook.com>
2023-08-28 15:12:32 -0300

    :construction: leitor qrcode :construction:

--- lib/src/dashboard/ponto/qr_code_ponto.dart
+++ lib/src/dashboard/ponto/qr_code_ponto.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 class QrCodePonto extends StatefulWidget {
   const QrCodePonto({super.key});
@@ -65,7 +66,19 @@ class _QrCodePontoState extends State<QrCodePonto> {
                         backgroundColor: Colors.green,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8))),
-                    onPressed: readQRCode,
+                    onPressed: () async {
+                      var res = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const SimpleBarcodeScannerPage(),
+                          ));
+                      setState(() {
+                        if (res is String) {
+                          tokenQrCode = res;
+                        }
+                      });
+                    },
                     child: const Text(
                       'Escanear QR Code',
                       style: TextStyle(
